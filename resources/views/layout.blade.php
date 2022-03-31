192e9bac059d10fd8a56f0c3ece1b64357a8c4b9
hassnaelhaili <hassnaelhaili@gmail.com>
2022-03-31 17:37:11 +0000

    register and controller & route & form

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>App For Training</title>
+	<!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+</head>
+<body>
+
+	<div class="container">
+
+		
+
+		@yield('content')
+		
+	</div>
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// USER
+Route::get('/register',[RegisterController::class,'index'])->name('sayft_form');
+
+Route::post('/register',[RegisterController::class,'register'])->name('register');
