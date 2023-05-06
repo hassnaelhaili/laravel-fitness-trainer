313d81e24ea7d8ff9764a8f00f3912a4c46a89af
Mourad Afakrouch <mourad.afakrouch@gmail.com>
2023-05-06 18:10:10 +0100

    Update program.php

--- app/Models/program.php
+++ app/Models/program.php
@@ -9,16 +9,9 @@ class Program extends Model
 {
     use HasFactory;
 
-    protected $guarded = [];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
+    public function client(){
+        return $this->belongsTo(Client::class);
     }
 
-    public function exersices()
-    {
-        return $this->hasMany(Exersice::class);
-    }
- 
 }
